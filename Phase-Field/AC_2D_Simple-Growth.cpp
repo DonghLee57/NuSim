86c2cc163791dbcaab6a807f2340cc32fec3abab
Dongheon <zwqa1234@gmail.com>
2023-09-21 00:28:34 +0900

    Update AC_2D_Simple-Growth.cpp

--- Phase-Field/AC_2D_Simple-Growth.cpp
+++ Phase-Field/AC_2D_Simple-Growth.cpp
@@ -2,23 +2,23 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <iomanip>
 #include <string>
 #include <cmath>
 #include <random>
-#include <omp.h>
+#include <mpi.h>
 using namespace std;
 
 // Discretization parameters
-const int MAXIT =  300;    // Iteration number
-const int FROMIT = 0;      // Continue from FROMIT
+const int MAXIT = 1000;    // Iteration number
 const int NSTEP =  100;    // Step size for save
 const int Ngr=  10;        // The number of grains
 const int Nx = 100;        // Discretization x-direction
 const int Ny = 100;        // Discretization y-direction
 double dx = 1;             // Grid spacing in x-direction [Ang]
 double dy = 1;             // Grid spacing in y-direction [Ang]
-double dt = 0.01;          // Size of time step [s]
-double Mob=  10.0;             // Mobility
+double dt = 0.005;          // Size of time step [ns]
+double Mob=  10.0;             // Mobility [Angs^3/(eV/ns)]
 double grad_coeff = 0.1;       // gradient coefficient
 double A = 1, B = 1, Gam = 1;
 const int BCells = 1; 	       // Number of boundary cells
@@ -29,20 +29,27 @@ const int My = Ny + 2 * BCells;
 
 double ETAS     [Ngr][Mx][My] = {0};
 double ETAS_new [Ngr][Mx][My] = {0};
-double df       [Mx][My] = {0};
+double df         = 0;
 double Lap_ETAS   = 0;
 double SUM_eta_sq = 0;
-int    MAX_Ngr    = Ngr;
 
 // Functions
 int check_stab();
 double lap_2D(double Grid[Mx][My], int x, int y, double dx, double dy, int Nx, int Ny);
 void SAVE(int step, int Ngr);
-void LOAD(int step, int Ngr);
 
-int main()
+int main(int argc, char **argv)
 {
-    check_stab();
+    int rank, size, psize;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    psize = Nx / size;
+
+    if (!check_stab()){
+        MPI_Finalize();
+        return 1;
+    }
 
     // Initiallization
     srand(230917);
@@ -59,42 +66,65 @@ int main()
         } 
     }
 
-    // Load data
-    if (FROMIT > 0) {
-        for (int n=0; n < Ngr; n++)
-            LOAD(FROMIT, n);
-    }
-
     // Evolve
-    #pragma omp parallel
-    //int N_THR = omp_get_num_threads();
-    cout << to_string(omp_get_num_threads()) << " threads\n";
-    for (int k=FROMIT+1; k <= FROMIT+MAXIT; k++) {
+    for (int k=0; k <= MAXIT; k++) {
         // Save data
-        if (k % NSTEP == 0){
-            printf("%d/%d\n",k,FROMIT+MAXIT);
+        if (k % NSTEP == 0 && rank == 0){
+            printf("%d/%d\n",k,MAXIT);
             for (int n=0; n < Ngr; n++){
-            SAVE(k, n);
+                SAVE(k, n);
             }
         }
         // Calculate
         for (int N=0; N < Ngr; N++){
-            for (int x=1; x<Nx+1; x++) {
-            for (int y=1; y<Ny+1; y++) {
-                SUM_eta_sq = 0;
-                for (int ngr=0; ngr < Ngr; ngr++){
-                    if (N != ngr) {SUM_eta_sq = SUM_eta_sq + pow(ETAS[ngr][x][y],2);}
+            if (rank == size - 1){
+                for (int x=rank*psize+1; x<Nx+1; x++) {
+                for (int y=1; y<Ny+1; y++) {
+                    SUM_eta_sq = 0;
+                    for (int ngr=0; ngr < Ngr; ngr++) SUM_eta_sq = SUM_eta_sq + pow(ETAS[ngr][x][y],2);
+                    df = -A*ETAS[N][x][y] + B*pow(ETAS[N][x][y],3) + 2*ETAS[N][x][y]*(SUM_eta_sq - pow(ETAS[N][x][y],2));
+                    Lap_ETAS = lap_2D(ETAS[N], x, y, dx, dy, Nx, Ny);
+                    ETAS_new[N][x][y] = ETAS[N][x][y] - dt*Mob*(df - grad_coeff*Lap_ETAS);
                 }
-                df[x][y] = -A*ETAS[N][x][y] + B*pow(ETAS[N][x][y],3) + 2*ETAS[N][x][y]*SUM_eta_sq;
+                }
+            } else {
+                for (int x=rank*psize+1; x<(rank+1)*psize+1; x++) {
+                for (int y=1; y<Ny+1; y++) {
+                    SUM_eta_sq = 0;
+                    for (int ngr=0; ngr < Ngr; ngr++) SUM_eta_sq = SUM_eta_sq + pow(ETAS[ngr][x][y],2);
+                    df = -A*ETAS[N][x][y] + B*pow(ETAS[N][x][y],3) + 2*ETAS[N][x][y]*(SUM_eta_sq - pow(ETAS[N][x][y],2));
+                    Lap_ETAS = lap_2D(ETAS[N], x, y, dx, dy, Nx, Ny);
+                    ETAS_new[N][x][y] = ETAS[N][x][y] - dt*Mob*(df - grad_coeff*Lap_ETAS);
+                }
+                }
             }
+            // MPI comm
+            MPI_Barrier(MPI_COMM_WORLD);
+            if (rank == size-1){
+                for (int x=rank*psize+1; x<Nx+1; x++) 
+                MPI_Send(ETAS_new[N][x], sizeof(ETAS_new[N][x])/sizeof(double), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+            } else {
+                for (int x=rank*psize+1; x<(rank+1)*psize+1; x++) 
+                MPI_Send(ETAS_new[N][x], sizeof(ETAS_new[N][x])/sizeof(double), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
             }
-            for (int x=1; x<Nx+1; x++){
-            for (int y=1; y<Ny+1; y++){ 
-                Lap_ETAS = lap_2D(ETAS[N], x, y, dx, dy, Nx, Ny);
-                ETAS_new[N][x][y] = ETAS[N][x][y] - dt*Mob*(df[x][y] - grad_coeff*Lap_ETAS);
+
+            MPI_Barrier(MPI_COMM_WORLD);
+            if (rank == 0) {
+                for (int r = 1; r < size; r++) {
+                    if (r == size-1) {
+                        for (int x=r*size+1; x<Nx+1; x++)
+                        MPI_Recv(ETAS_new[N][x], sizeof(ETAS_new[N][x])/sizeof(double), MPI_DOUBLE, r, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    } else {
+                        for (int x=r*size+1; x<(r+1)*size+1; x++)
+                        MPI_Recv(ETAS_new[N][x], sizeof(ETAS_new[N][x])/sizeof(double), MPI_DOUBLE, r, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    }
+                }
+                for (int n=0; n<Nx+1; n++)
+                MPI_Bcast(ETAS_new[n], sizeof(ETAS_new[n])/sizeof(double), MPI_DOUBLE, 0, MPI_COMM_WORLD);
             }
-            }
+            MPI_Barrier(MPI_COMM_WORLD);
         } // Ngr loop
+
 
         // Periodic boundary condition
         for (int N=0; N < Ngr; N++){
@@ -113,8 +143,10 @@ int main()
         for (int x=0; x<Mx; x++) 
         for (int y=0; y<My; y++)
             ETAS[N][x][y] = ETAS_new[N][x][y];
-        #pragma omp barrier
     } // MAXIT loop
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Finalize();
 
     return 0; 
 }
@@ -143,36 +175,80 @@ double lap_2D(double Grid[Mx][My], int x, int y, double dx, double dy, int Nx, i
 void SAVE(int step, int Ngr)
 {
     ofstream myfile("PhaseField_" + to_string(step) + "_" + to_string(Ngr)+ ".dat");
-    int NSITE = 0;
     for (int x=1; x<Nx+1; x++) {
         for (int y=1; y<Ny+1; y++) {
-        if (ETAS[Ngr][x][y] > 0.9) {NSITE = NSITE + 1;}
-        myfile << double(ETAS[Ngr][x][y]);
+        myfile << fixed << setprecision(4) << double(ETAS[Ngr][x][y]);
         if (y < Ny) { myfile << "  ";}
         }
         myfile << "\n";
     }
-    if (NSITE == 0) {
-        MAX_Ngr = MAX_Ngr - 1;
-        printf("Effective %d grains are in my system.\n", MAX_Ngr);
-    }
     myfile.close();
 }
 
-void LOAD(int step, int Ngr)
+/*
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cmath>
+#include <random>
+#include <mpi.h>
+using namespace std;
+
+int main(int argc, char **argv)
 {
-    ifstream myfile("PhaseField_" + to_string(step) + "_" + to_string(Ngr)+ ".dat");
-    int NSITE = 0;
-    for (int x=1; x<Nx+1; x++) {
-        for (int y=1; y<Ny+1; y++) {
-        myfile >> ETAS[Ngr][x][y];
-        if (ETAS[Ngr][x][y] > 0.9) {NSITE = NSITE + 1;}
+    int rank, size, psize;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    psize = 4 / size;
+
+    int MAT[4][4] = {0};
+
+    for (int n=rank*psize; n<(rank+1)*psize; n++){
+    printf(" %d ", rank);
+    for (int m=0; m<4; m++){
+        MAT[n][m] = rank;
+        printf(" %d ", MAT[n][m]);
+    }
+    printf(" \n ");
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    printf(" \n ");
+    
+    //for (int r = 1; r < size; r++) {
+        //for (int n=r*size; n<(r+1)*size; n++){
+        for (int n=rank*size; n<(rank+1)*size; n++){
+            MPI_Send(MAT[n], sizeof(MAT[n])/sizeof(int), MPI_INT, 0, 0, MPI_COMM_WORLD);
         }
-        myfile << "\n";
+    //}
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if (rank ==0){
+    for (int r = 1; r < size; r++) {
+        for (int n=r*size; n<(r+1)*size; n++){
+        MPI_Recv(MAT[n], sizeof(MAT[n])/sizeof(int), MPI_INT, r, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
     }
-    if (NSITE == 0) {
-        MAX_Ngr = MAX_Ngr - 1;
-        printf("Effective %d grains are in my system.\n", MAX_Ngr);
+    for (int n=0; n<4; n++){
+        MPI_Bcast(MAT[n], sizeof(MAT[n])/sizeof(int), MPI_INT, 0, MPI_COMM_WORLD);
     }
-    myfile.close();
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+
+
+    for (int n=0; n<4; n++){
+    printf(" %d ", rank);
+    for (int m=0; m<4; m++){
+        printf(" %d ", MAT[n][m]);
+    }
+    printf(" \n ");
+    }
+
+
+    MPI_Finalize();
+    return 0; 
 }
+*/
