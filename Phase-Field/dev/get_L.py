d0d1a4352514aff8f2eef88575e32e568efda56d
Dongheon <zwqa1234@gmail.com>
2023-12-21 18:38:10 +0900

    Create get_L.py

---
+++ Phase-Field/dev/get_L.py
@@ -0,0 +1,24 @@
+# ACS Omega 8, 3329 (2023)
+
+# gamma: interfacial energy
+# delta: interfacial width
+# b: ~2.2
+
+def L(T):
+    A = 1
+    B = 1
+    T_melt = 1000
+    delta_Tr = (1-T/T_melt)
+    spacing = 2.4
+    return A*(D(T)*delta_Tr/(2*np.pi*spacing**2))**B
+
+def K():
+    return 12*delta*gamma/b
+
+def W():
+    return 6*gamma*b/delta
+
+def D(T):
+    D0 = 1E-4 # cm2/s
+    Ea = 2.0  # eV
+    return D0*np.exp(-Ea/(kb_eV*T))
