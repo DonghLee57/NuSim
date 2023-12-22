75e7b221cce23ea5b1964bee2fc7898d9fa1f7de
Dongheon <zwqa1234@gmail.com>
2023-12-22 15:01:25 +0900

    Update and rename CNT.py to Classical_nucleation.py

--- Phase-Field/dev/Classical_nucleation.py
+++ Phase-Field/dev/Classical_nucleation.py
@@ -2,16 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import constants
 kb_eV = constants.k/constants.e # eV/K
+# Si molar volume
+Vmolar = 1.205883E-5*1E+27 # nm^3/mol
 
 T_melt= 1685 # K
 gamma = 0.45/constants.e/1E+18 # eV/nm^2
-# J/m^2 -> eV/m^2 -> eV/nm^2
-#2.81 # eV/nm^2
-dHc   = 0.12 # eV
-dHc   = 11.9/constants.e/constants.N_A # eV/nm^3
-#dHc   = 11.9/constants.e/constants.N_A/2.555*1E+3 # eV/nm^3
-# J/mol -> eV/mol -> eV/atom -> eV/angs3 -> eV/nm^3
-print(dHc)
+print(f"Interfacial energy: {gamma:.2f} eV/nm^2")
+dHc   = 11900/constants.e/Vmolar # eV/nm^3 Heat of crystallization
+#dHc   = 50550/constants.e/Vmolar # eV/nm^3 Heat of fusion
+# J/mol -> eV/mol -> eV/nm^3
+print(f"Latent heat of crystallization: {dHc:.2f} eV/nm^3")
 
 def dG(T):
     return dHc*(1-T/T_melt)
@@ -27,17 +27,13 @@ T = np.linspace(500,1500)
 ax[0].plot(T, dGstar(T))
 #print(f'RTA: Linear size ~ {(growth(T)/rate_nuc(T))**(0.25)*1E+9:.2f} nm')
 ax[0].set_ylabel('Nucleation barrier (eV)')
-#ax[0].set_ylim([0, 1])
 ax[0].set_xlabel('Temperature (K)')
-#ax[0].set_xlim(left=0)
-#ax[0].legend()
+ax[0].set_ylim(bottom=0)
 
-ax[1].plot(T, r_crt(T))
-ax[1].set_ylabel('Critical radius (nm)')
+ax[1].plot(T, 2*r_crt(T))
+ax[1].set_ylabel('Critical size (nm)')
 ax[1].set_xlabel('Temperature (K)')
-#ax[1].set_ylim([0, 1])
-#ax[1].set_xlabel('time (hour)')
-#ax[1].set_xlim(left=0)
-#ax[1].legend()
+ax[1].set_ylim(bottom=0)
+
 plt.tight_layout()
 plt.show()
