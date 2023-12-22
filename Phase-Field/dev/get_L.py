6246442f0eab668835e51559b528a93a0bf6de58
Dongheon <zwqa1234@gmail.com>
2023-12-22 11:27:11 +0900

    Update get_L.py

--- Phase-Field/dev/get_L.py
+++ Phase-Field/dev/get_L.py
@@ -4,13 +4,24 @@
 # delta: interfacial width
 # b: ~2.2
 
+def D(T):
+    D0 = 1E-4 # cm2/s
+    Ea = 2.0  # eV
+    return D0*np.exp(-Ea/(kb_eV*T))
+
+def A(T):
+    # Relation between A and D
+    # A = -2.0E-22*D**(-0.78)
+    return -2.0E-22*D(T)**(-0.78) 
+
 def L(T):
-    A = 1 # m^4/(J*s)
-    B = 1 # unitless
+    # Unit of L: distance^3/energy/time
+    #A = 1 # m^4/(J*s)
+    B = 1 # unitless (0.6~1.4)
     T_melt = 1000
     delta_Tr = (1-T/T_melt)
-    spacing = 2.4 # angstrom
-    return A*(D(T)*delta_Tr/(2*np.pi*spacing**2))**B
+    spacing = 2.4E-10 # m
+    return A(T)*(D(T)*delta_Tr/(2*np.pi*spacing**2))**B
 
 def K():
     return 12*delta*gamma/b
@@ -18,7 +29,3 @@ def K():
 def W():
     return 6*gamma*b/delta
 
-def D(T):
-    D0 = 1E-4 # cm2/s
-    Ea = 2.0  # eV
-    return D0*np.exp(-Ea/(kb_eV*T))
