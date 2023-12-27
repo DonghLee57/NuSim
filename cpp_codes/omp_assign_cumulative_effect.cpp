67a7ac4bc36de64b85f835a3eaba331e23088932
Dongheon <zwqa1234@gmail.com>
2023-12-27 21:04:26 +0900

    Update omp_assign_cumulative_effect.cpp

--- cpp_codes/omp_assign_cumulative_effect.cpp
+++ cpp_codes/omp_assign_cumulative_effect.cpp
@@ -22,7 +22,7 @@ int main(){
     }
 
     // OpenMP assignment
-    cout << "OpenMP - assign 2D matrix without private" << endl;
+    cout << "OpenMP - assign 2D matrix" << endl;
     vec = {1,2,3,4,5,6,7,8,9,10};
     #pragma omp parallel for
     for (int i=0; i<nrow; i++){
@@ -40,7 +40,7 @@ int main(){
     To achieve the same result in the OpenMP block as in the simple loop block,
     while avoiding race conditions, it's important to understand how the data is being modified and accessed by multiple threads.
     */
-    cout << "OpenMP - assign 2D matrix with private" << endl;
+    cout << "OpenMP - assign 2D matrix changing algorithm to avoid the cumulative effect" << endl;
     vec = {1,2,3,4,5,6,7,8,9,10};
     #pragma omp parallel for
     for (int i=0; i<nrow; i++){
