aec10a190326d6e55e224444e958792e85679339
Dongheon <zwqa1234@gmail.com>
2023-12-27 23:42:29 +0900

    Create step-by-step.cpp

---
+++ Phase-Field/dev/step-by-step.cpp
@@ -0,0 +1,272 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <cmath>
+#include <random>
+#include <algorithm>
+#include <omp.h>
+using namespace std;
+
+constexpr double PI = M_PI;
+constexpr double q = 1.602177e-19;         // [C]
+constexpr double kb_eV = 1.380649e-23 / q; // [eV/K]
+constexpr double Tmelt = 1685;             // [K]
+constexpr double Vm = 1.205883199E-5;      // [m3/mol]
+constexpr double GAMMA = 0.38/q;           // Interfacial energy [eV/m2]
+constexpr double delta = 2E-10;            // Interfacial width  [m]
+constexpr double b = 2.2;                  // ~2.2
+constexpr double dHf = 50500/q/Vm;         // [eV/m3]
+constexpr double K = 12*delta*GAMMA/b;     // Gradient coefficient [eV/m]
+constexpr double W = 6*GAMMA*b/delta;      // [eV/m3]
+
+const double Tsim = 1273; // [K]
+const int DIM = 3;
+const int MAXIT =    10000;
+const int NSTEP =    1;
+const int NMAX  = 4;
+const int Nx = 8;
+const int Ny = 8;
+const int Nz = 1;
+const int BCells = 1;
+
+const double dx = 1E-10, dy = 1E-10, dz = 1E-10;  // Grid spacing in x-,y-,z-direction [m]
+const double dt = 100E-9;                         // Size of time step [s]
+const double Mob = 2.8E-24;                       // Mobility [m^3/(eV/s)]
+double Lap_ETAS = 0;
+double df= 0;
+
+// Phase-field storage
+const int Mx = Nx + 2 * BCells;
+const int My = Ny + 2 * BCells;
+const int Mz = Nz + 2 * BCells;
+
+using PhaseField3D = vector<vector<vector<double>>>;
+
+PhaseField3D ETAS_TOT(Mx, vector<vector<double>>(My, vector<double>(Mz, 0.0)));
+vector<PhaseField3D> ETAS(NMAX, ETAS_TOT);
+vector<PhaseField3D> ETAS_new(NMAX, ETAS_TOT);
+
+// Function declarations
+int CHECK_STABILITY(int dim);
+double LAPLACIAN(const PhaseField3D& Grid, int x, int y, int z);
+int COUNT_AMOR(const PhaseField3D& Grid);
+double PROB_NUC(double T, double Va);
+double GR_VEL(double T);
+void SAVE_2D(int step, int NMAX, int z);
+void check_grid(const PhaseField3D& Grid);
+void check_prob_nuc();
+
+int main()
+{
+    int Ngr = 0;
+    int Ca;
+    double Va, Xa;
+    double distance;
+
+
+    //if (!CHECK_STABILITY(DIM)) {
+    //    return 1;
+    //}
+
+    // Initialization
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> distrX(1, Nx);
+    uniform_int_distribution<int> distrY(1, Ny);
+    uniform_int_distribution<int> distrZ(1, Nz);
+    uniform_real_distribution<double> probDist(0.0, 1.0);
+
+    int rx = distrX(gen);
+    int ry = distrY(gen);
+    int rz = distrZ(gen);
+    #pragma omp parallel for private(distance)
+    for (int x = 1; x < Nx + 1; ++x) {
+        for (int y = 1; y < Ny + 1; ++y) {
+            for (int z = 1; z < Nz + 1; ++z) {
+                distance = sqrt((rx - x) * (rx - x) + (ry - y) * (ry - y) + (rz - z) * (rz - z));
+                ETAS[Ngr][x][y][z] = (distance < 1.2) ? 1.0 : 0.0;
+		// Test
+                //ETAS[Ngr][x][y][z] = omp_get_thread_num();
+            }
+        }
+    }
+    ETAS_TOT = ETAS[Ngr];
+    // Test
+    //cout << "Grain 0" << endl;
+    //check_grid(ETAS[Ngr]);
+    cout << "TOT" << endl;
+    check_grid(ETAS_TOT);
+
+    Ngr++;
+    // Evolve
+    for (int k = 0; k <= MAXIT; ++k) {
+        /*
+        // Save data
+        if (k % NSTEP == 0) {
+            cout << k << '/' << MAXIT << '\n';
+            for (int n = 0; n < NMAX; ++n) {
+                SAVE_2D(k, n, 0);
+            }
+        }
+	*/
+        // Test
+        Ca = COUNT_AMOR(ETAS_TOT);
+        Va = Ca * dx * dy * dz;
+        Xa = (double) Ca / (Nx * Ny * Nz);
+        cout << "No. of Amor.: " << Ca << endl;
+        cout << "Amor. Vol.: " << Va << " (m^3)"  << endl;
+        cout << "Amor. Fraction: " << Xa << endl;
+
+    //check_prob_nuc();
+
+        //if (PROB_NUC(Tsim, Va*1E+15) > probDist(gen) && Xa < 0.9 && Ngr < NMAX) {
+        if (PROB_NUC(Tsim, 1E-16) && Ngr < NMAX) {// > probDist(gen) && Xa < 0.9 && Ngr < NMAX) {
+            rx = distrX(gen);
+            ry = distrY(gen);
+            rz = distrZ(gen);
+
+            while (ETAS_TOT[rx][ry][rz] > 0.1) {
+                rx = distrX(gen);
+                ry = distrY(gen);
+                rz = distrZ(gen);
+            }
+            #pragma omp parallel for private(distance)
+            for (int x = 1; x < Nx + 1; ++x) {
+                for (int y = 1; y < Ny + 1; ++y) {
+                    for (int z = 1; z < Nz + 1; ++z) {
+                        distance = sqrt((rx - x) * (rx - x) + (ry - y) * (ry - y) + (rz - z) * (rz - z));
+                        ETAS[Ngr][x][y][z] = (distance < 1.2) ? 1.0 : 0.0;
+			ETAS_TOT[x][y][z] = ETAS_TOT[x][y][z] + ETAS[Ngr][x][y][z];
+                    }
+                }
+            }
+            Ngr++;
+        }
+    } 
+    check_grid(ETAS_TOT);
+    for (int N = 0; N < Ngr; N++){
+    cout << "Grain " << N <<  endl;
+    check_grid(ETAS[N]);
+    }
+    /*
+
+        // Calculate
+        for (int N = 0; N < Ngr; ++N) {
+        #pragma omp parallel for
+        for (int x = 1; x < Nx + 1; ++x) {
+            for (int y = 1; y < Ny + 1; ++y) {
+                for (int z = 1; z < Nz + 1; ++z) {
+                    double SUM_eta_sq = 0;
+                    for (int ngr = 0; ngr < Ngr; ++ngr) {
+                        SUM_eta_sq += pow(ETAS[ngr][x][y][z], 2);
+                    }
+                    df = -A * ETAS[N][x][y][z] + B * pow(ETAS[N][x][y][z], 3) +
+                         2 * ETAS[N][x][y][z] * (SUM_eta_sq - pow(ETAS[N][x][y][z], 2));
+                    Lap_ETAS = LAPLACIAN(ETAS[N], x, y, z);
+                    ETAS_new[N][x][y][z] = ETAS[N][x][y][z] - dt * Mob * (df - K * Lap_ETAS);
+                }
+            }
+        }
+        }
+        // Update ETAS
+        ETAS = ETAS_new;
+    } // MAXIT loop
+    */
+    return 0;
+}
+
+int CHECK_STABILITY(int dim)
+{
+    float res = (Mob * dt) * (1 / (dx * dx) + 1 / (dy * dy) + 1 / (dz * dz));
+    float crit = 1.0 / 2.0 / dim;
+    if (res < crit) {
+        cout << "Maybe stable. " << res << " is less than " << crit << ".\n";
+        return 1;
+    } else {
+        cout << "Maybe unstable. " << res << " is greater than " << crit << ".\n";
+        return 0;
+    }
+}
+
+double LAPLACIAN(const PhaseField3D& Grid, int x, int y, int z)
+{
+    int idx0 = x - 1, idxN = x + 1;
+    int idy0 = y - 1, idyN = y + 1;
+    int idz0 = z - 1, idzN = z + 1;
+
+    return (Grid[idx0][y][z] + Grid[idxN][y][z] - 2 * Grid[x][y][z]) / (dx * dx) +
+           (Grid[x][idy0][z] + Grid[x][idyN][z] - 2 * Grid[x][y][z]) / (dy * dy) +
+           (Grid[x][y][idz0] + Grid[x][y][idzN] - 2 * Grid[x][y][z]) / (dz * dz);
+}
+
+int COUNT_AMOR(const PhaseField3D& Grid)
+{
+    int count = 0;
+    #pragma omp parallel for reduction(+ : count)
+    for (int x = 1; x < Nx + 1; ++x) {
+        for (int y = 1; y < Ny + 1; ++y) {
+            for (int z = 1; z < Nz + 1; ++z) {
+                if (Grid[x][y][z] < 0.5) {
+                    ++count;
+                }
+            }
+        }
+    }
+    return count;
+}
+
+double PROB_NUC(double T, double Va)
+{
+    // Mater. Res. Soc. Symp. Proc. 989, 0989-A06-17 (2007)
+    double beta = 1 / (kb_eV * T);
+    double I0 = 1.7e+44; // [m-3sec-1]
+    double Ea = 5.3;     // [eV]
+    double I_nuc = I0 * exp(-beta * Ea);
+    return 1 - exp(-I_nuc * Va * dt);
+}
+
+double GR_VEL(double T)
+{
+    // Mater. Res. Soc. Symp. Proc. 989, 0989-A06-17 (2007)
+    double beta = 1 / (kb_eV * T);
+    double u0 = 2.1e+7; // [m/s]
+    double Ea = 3.1;    // [eV]
+    return u0 * exp(-beta * Ea);
+}
+
+void SAVE_2D(int step, int NMAX, int z)
+{
+    ofstream myfile("PhaseField_" + to_string(step) + "_" + to_string(NMAX) + "_" + to_string(z) + ".dat");
+    for (int x = 1; x < Nx + 1; ++x) {
+        for (int y = 1; y < Ny + 1; ++y) {
+            myfile << fixed << setprecision(4) << ETAS[NMAX][x][y][z];
+            if (y < Ny) {
+                myfile << "  ";
+            }
+        }
+        myfile << "\n";
+    }
+    myfile.close();
+}
+
+
+void check_grid(const PhaseField3D& Grid)
+{
+    for (int x = 1; x < Nx + 1; ++x) {
+        for (int y = 1; y < Ny + 1; ++y) {
+            for (int z = 1; z < Nz + 1; ++z) {
+                cout << Grid[x][y][z] << " ";
+            }
+        }
+	cout << endl;
+    }
+}
+
+void check_prob_nuc()
+{
+    for (int k = 0; k <=100; k++){
+        cout << PROB_NUC(Tsim,1E-16) << " ";
+    }
+    cout << endl;
+}
