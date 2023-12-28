b5e5f262d10a0f0d4f3f1fbc7a41555a2eb5ebb2
Dongheon <zwqa1234@gmail.com>
2023-12-28 12:50:32 +0900

    Create swap_vector_orders.cpp

---
+++ cpp_codes/swap_vector_orders.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<vector<vector<int>>> MAT3D;
+    MAT3D = {{{1,2,3},{4,5,6},{7,8,9}},
+             {{0,0,0},{0,0,0},{0,0,0}}};
+
+    // Print initial state
+    cout << "Initial state" << endl;
+    for (int i=0; i < MAT3D.size(); i++){
+        for (int j=0; j < MAT3D[i].size(); j++){
+            for (int k=0; k < MAT3D[i][j].size(); k++){
+                cout << MAT3D[i][j][k] << " ";
+            }   
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+    // Using 'swap' function in std
+    swap(MAT3D[0],MAT3D[1]);
+  
+    // Print final state after swap
+    cout << "Final state" << endl;
+    for (int i=0; i < MAT3D.size(); i++){
+        for (int j=0; j < MAT3D[i].size(); j++){
+            for (int k=0; k < MAT3D[i][j].size(); k++){
+                cout << MAT3D[i][j][k] << " ";
+            }   
+            cout << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}
