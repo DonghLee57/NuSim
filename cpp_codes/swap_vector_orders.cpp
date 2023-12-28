9f1e81e13eab987abbf856db7b591dae691bee08
Dongheon <zwqa1234@gmail.com>
2023-12-28 14:18:39 +0900

    Update swap_vector_orders.cpp

--- cpp_codes/swap_vector_orders.cpp
+++ cpp_codes/swap_vector_orders.cpp
@@ -22,8 +22,6 @@ int main(){
 
     // Using 'swap' function in std
     swap(MAT3D[0],MAT3D[1]);
-    
-  
     // Print final state after swap
     cout << "Final state" << endl;
     for (int i=0; i < MAT3D.size(); i++){
@@ -36,9 +34,10 @@ int main(){
         cout << endl;
     }
 
+    // Using 'swap' function in a user-defined function
     my_swap(MAT3D);
     // Print final state after swap
-    cout << "Final state" << endl;
+    cout << "Final state 2" << endl;
     for (int i=0; i < MAT3D.size(); i++){
         for (int j=0; j < MAT3D[i].size(); j++){
             for (int k=0; k < MAT3D[i][j].size(); k++){
@@ -54,3 +53,32 @@ int main(){
 void my_swap(vector<vector<vector<int>>>& MAT){
     swap(MAT[0],MAT[1]);
 }
+
+/* - Example Output -
+Initial state
+1 2 3 
+4 5 6 
+7 8 9 
+
+0 0 0 
+0 0 0 
+0 0 0 
+
+Final state
+0 0 0 
+0 0 0 
+0 0 0 
+
+1 2 3 
+4 5 6 
+7 8 9 
+
+Final state 2
+1 2 3 
+4 5 6 
+7 8 9 
+
+0 0 0 
+0 0 0 
+0 0 0 
+*/
