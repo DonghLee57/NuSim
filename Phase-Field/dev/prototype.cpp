85c49a392586140bb3ead2bccf79e347a1cb4b6c
Dongheon <zwqa1234@gmail.com>
2023-12-29 14:31:08 +0900

    Update prototype.cpp

--- Phase-Field/dev/prototype.cpp
+++ Phase-Field/dev/prototype.cpp
@@ -53,12 +53,10 @@ vector<PhaseField3D> ETAS(NMAX, ETAS_TOT);
 vector<PhaseField3D> ETAS_new(NMAX, ETAS_TOT);
 
 // Function declarations
-int CHECK_STABILITY(int dim);
 double LAPLACIAN(const PhaseField3D& Grid, int x, int y, int z);
 int COUNT_AMOR(const PhaseField3D& Grid);
 double PROB_NUC(double T, double Va);
 void SAVE_2D(int step, int NMAX, int z);
-int SORT_GRAIN(vector<PhaseField3D>& NGrid, int Ngr);
 void check_grid(const PhaseField3D& Grid);
 
 int main()
@@ -68,10 +66,6 @@ int main()
     double Va, Xa;
     double distance;
     double SUM_eta_sq = 0;
-
-    //if (!CHECK_STABILITY(DIM)) {
-    //    return 1;
-    //}
 
     // Initialization
     random_device rd;
@@ -116,7 +110,7 @@ int main()
         cout << "Amor. Vol.: " << Va << " (m^3)"  << endl;
         cout << "Amor. Fraction: " << Xa << endl;
 
-        if (PROB_NUC(Tsim, Va) > probDist(gen) && Xa > 0.1 && Ngr < NMAX) {
+        if (PROB_NUC(Tsim, Va) > probDist(gen) && Xa > 0.1) {
             rx = distrX(gen);
             ry = distrY(gen);
             rz = distrZ(gen);
@@ -125,17 +119,31 @@ int main()
                 ry = distrY(gen);
                 rz = distrZ(gen);
             }
-            #pragma omp parallel for private(distance)
-            for (int x = 1; x < Nx + 1; ++x) {
+            if  ( Ngr < NMAX ) {
+                #pragma omp parallel for private(distance)
+                for (int x = 1; x < Nx + 1; ++x) {
                 for (int y = 1; y < Ny + 1; ++y) {
-                    for (int z = 1; z < Nz + 1; ++z) {
-                        distance = sqrt((rx - x) * (rx - x) + (ry - y) * (ry - y) + (rz - z) * (rz - z));
-                        ETAS[Ngr][x][y][z] = (distance < Rc) ? 1.0 : 0.0;
-			ETAS_TOT[x][y][z] = ETAS_TOT[x][y][z] + ETAS[Ngr][x][y][z];
+                for (int z = 1; z < Nz + 1; ++z) {
+                    distance = sqrt((rx - x) * (rx - x) + (ry - y) * (ry - y) + (rz - z) * (rz - z));
+                    ETAS[Ngr][x][y][z] = (distance < Rc) ? 1.0 : 0.0;
+                    ETAS_TOT[x][y][z] = ETAS_TOT[x][y][z] + ETAS[Ngr][x][y][z];
+                }}}
+                Ngr++;
+            } else {
+                for (int N=0; N < Ngr; N++) {
+                    Ca = COUNT_AMOR(ETAS[N]);
+                    if ( Nx*Ny*Nz - Ca < 100 ){
+                        for (int x = 1; x < Nx + 1; ++x) {
+                        for (int y = 1; y < Ny + 1; ++y) {
+                        for (int z = 1; z < Nz + 1; ++z) {
+                            distance = sqrt((rx - x) * (rx - x) + (ry - y) * (ry - y) + (rz - z) * (rz - z));
+                            ETAS[N][x][y][z] = (distance < Rc) ? 1.0 : 0.0;
+                            ETAS_TOT[x][y][z] = ETAS_TOT[x][y][z] + ETAS[N][x][y][z];
+                        }}}
                     }
+                    break;
                 }
             }
-            Ngr++;
         }
 
         // Calculate
@@ -162,45 +170,45 @@ int main()
         for (int N = 0; N < Ngr; ++N) {
             if (PBCX == 'p'){
                 //#pragma omp critical
-                for (int y = 1; y < Ny + 1; ++y) {
-                for (int z = 1; z < Nz + 1; ++z) {
+                for (int y = 0; y < My; ++y) {
+                for (int z = 0; z < Mz; ++z) {
                     ETAS_new[N][0][y][z] = ETAS_new[N][Nx][y][z];
                     ETAS_new[N][Nx+1][y][z] = ETAS_new[N][1][y][z];
                 }}
             } else if (PBCX == 'r'){
                 #pragma omp parallel for
-                for (int y = 1; y < Ny + 1; ++y) {
-                for (int z = 1; z < Nz + 1; ++z) {
+                for (int y = 0; y < My; ++y) {
+                for (int z = 0; z < Mz; ++z) {
                     ETAS_new[N][0][y][z] = ETAS_new[N][1][y][z];
                     ETAS_new[N][Nx+1][y][z] = ETAS_new[N][Nx][y][z];
                 }}
             }
             if (PBCY == 'p'){
                 #pragma omp parallel for
-                for (int x = 1; x < Nx + 1; ++x) {
-                for (int z = 1; z < Nz + 1; ++z) {
+                for (int x = 0; x < Mx; ++x) {
+                for (int z = 0; z < Mz; ++z) {
                     ETAS_new[N][x][0][z] = ETAS_new[N][x][Ny][z];
                     ETAS_new[N][x][Ny+1][z] = ETAS_new[N][x][1][z];
                 }}
             } else if (PBCY == 'r'){
                 #pragma omp parallel for
-                for (int x = 1; x < Nx + 1; ++x) {
-                for (int z = 1; z < Nz + 1; ++z) {
+                for (int x = 0; x < Mx; ++x) {
+                for (int z = 0; z < Mz; ++z) {
                     ETAS_new[N][x][0][z] = ETAS_new[N][x][1][z];
                     ETAS_new[N][x][Ny+1][z] = ETAS_new[N][x][Ny][z];
                 }}
             }
             if (PBCZ == 'p'){
                 #pragma omp parallel for
-                for (int x = 1; x < Nx + 1; ++x) {
-                for (int y = 1; y < Ny + 1; ++y) {
+                for (int x = 0; x < Mx; ++x) {
+                for (int y = 0; y < My; ++y) {
                     ETAS_new[N][x][y][0] = ETAS_new[N][x][y][Nz];
                     ETAS_new[N][x][y][Nz+1] = ETAS_new[N][x][y][1];
                 }}
             } else if (PBCZ == 'r'){
                 #pragma omp parallel for
-                for (int x = 1; x < Nx + 1; ++x) {
-                for (int y = 1; y < Ny + 1; ++y) {
+                for (int x = 0; x < Mx; ++x) {
+                for (int y = 0; y < My; ++y) {
                     ETAS_new[N][x][y][0] = ETAS_new[N][x][y][1];
                     ETAS_new[N][x][y][Nz+1] = ETAS_new[N][x][y][Nz];
             }}
@@ -211,9 +219,9 @@ int main()
         //ETAS = ETAS_new;
         for (int N = 0; N < Ngr; ++N) {
         #pragma omp parallel for
-        for (int x = 1; x < Nx + 1; ++x) {
-        for (int y = 1; y < Ny + 1; ++y) {
-        for (int z = 1; z < Nz + 1; ++z) {
+        for (int x = 0; x < Mx; ++x) {
+        for (int y = 0; y < My; ++y) {
+        for (int z = 0; z < Mz; ++z) {
             if (N == 0){
                 ETAS_TOT[x][y][z] = ETAS_new[N][x][y][z];
             } else {
@@ -222,22 +230,9 @@ int main()
             ETAS[N][x][y][z] = ETAS_new[N][x][y][z];
         }}}}
         // position
-        Ngr = SORT_GRAIN(ETAS, Ngr);
+        //Ngr = SORT_GRAIN(ETAS, Ngr);
     } // MAXIT loop
     return 0;
-}
-
-int CHECK_STABILITY(int dim)
-{
-    float res = (Mob * dt) * (1 / (dx * dx) + 1 / (dy * dy) + 1 / (dz * dz));
-    float crit = 1.0 / 2.0 / dim;
-    if (res < crit) {
-        cout << "Maybe stable. " << res << " is less than " << crit << ".\n";
-        return 1;
-    } else {
-        cout << "Maybe unstable. " << res << " is greater than " << crit << ".\n";
-        return 0;
-    }
 }
 
 double LAPLACIAN(const PhaseField3D& Grid, int x, int y, int z)
@@ -271,8 +266,8 @@ double PROB_NUC(double T, double Va)
 {
     // Mater. Res. Soc. Symp. Proc. 989, 0989-A06-17 (2007)
     double beta = 1 / (kb_eV * T);
-    double I0 = 1.7e+44; // [m-3sec-1]
-    double Ea = 5.3;     // [eV]
+    double I0 = 1.0E+52; // [m-3sec-1]
+    double Ea = 1.6*PI*pow(GAMMA,3)/pow(dHf/(1-T/Tmelt),2)/3 + 5.7;     // [eV]
     double I_nuc = I0 * exp(-beta * Ea);
     return 1 - exp(-I_nuc * Va * dt);
 }
@@ -293,6 +288,32 @@ void SAVE_2D(int step, int NMAX, int z)
     myfile.close();
 }
 
+void check_grid(const PhaseField3D& Grid)
+{
+    for (int x = 0; x < Nx + 2; ++x) {
+        for (int y = 0; y < Ny + 2; ++y) {
+            //for (int z = 0; z < Nz + 2; ++z) {
+                cout << fixed << setprecision(2) << Grid[x][y][1] << " ";
+            //}
+        }
+        cout << endl;
+    }
+}
+
+/*
+int CHECK_STABILITY(int dim)
+{
+    float res = (Mob * dt) * (1 / (dx * dx) + 1 / (dy * dy) + 1 / (dz * dz));
+    float crit = 1.0 / 2.0 / dim;
+    if (res < crit) {
+        cout << "Maybe stable. " << res << " is less than " << crit << ".\n";
+        return 1;
+    } else {
+        cout << "Maybe unstable. " << res << " is greater than " << crit << ".\n";
+        return 0;
+    }
+}
+
 int SORT_GRAIN(vector<PhaseField3D>& NGrid, int Ngr){
     vector<PhaseField3D> sorted(NMAX, ETAS_TOT);
     int sorted_Ngr = 0;
@@ -309,19 +330,6 @@ int SORT_GRAIN(vector<PhaseField3D>& NGrid, int Ngr){
     return sorted_Ngr;
 }
 
-void check_grid(const PhaseField3D& Grid)
-{
-    for (int x = 0; x < Nx + 2; ++x) {
-        for (int y = 0; y < Ny + 2; ++y) {
-            //for (int z = 0; z < Nz + 2; ++z) {
-                cout << fixed << setprecision(2) << Grid[x][y][1] << " ";
-            //}
-        }
-        cout << endl;
-    }
-}
-
-/*
 double GR_VEL(double T)
 {
     // Mater. Res. Soc. Symp. Proc. 989, 0989-A06-17 (2007)
