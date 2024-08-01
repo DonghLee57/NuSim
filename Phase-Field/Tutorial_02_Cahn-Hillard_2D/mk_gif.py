4ad42c76cbaf0c79a479f9bd1f9185f52cd106cd
Dongheon <zwqa1234@gmail.com>
2024-08-01 23:16:43 +0900

    Update mk_gif.py

--- Phase-Field/Tutorial_02_Cahn-Hillard_2D/mk_gif.py
+++ Phase-Field/Tutorial_02_Cahn-Hillard_2D/mk_gif.py
@@ -2,7 +2,7 @@ import pyvista as pv
 
 pvd = pv.get_reader("simulation_results.pvd")
 sargs = dict(height=0.40, vertical=True, position_x= 0.8, position_y=0.4, n_labels=5, title='psi', fmt='%.2f')
-mesh_kwargs = {'scalars':'phase_field',
+mesh_kwargs = {'scalars':'psi',
               'scalar_bar_args':sargs,
               'clim':[0,1],
               'cmap':'bwr'}
