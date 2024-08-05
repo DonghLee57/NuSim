8fced6fb02e444161df38ceedb0a116929ec43f0
Dongheon <zwqa1234@gmail.com>
2024-08-05 19:38:21 +0900

    Update mk_gif.py

--- Phase-Field/Tutorial_02_Cahn-Hillard_2D/mk_gif.py
+++ Phase-Field/Tutorial_02_Cahn-Hillard_2D/mk_gif.py
@@ -8,7 +8,15 @@ mesh_kwargs = {'scalars':'psi',
               'cmap':'bwr'}
 
 p = pv.Plotter()
-p.open_gif("res.gif")
+fname = 'res.mp4'
+fformat = fname.split('.')[-1]
+if fformat == 'gif':
+    # pip install imageio
+    p.open_gif("res.gif")
+elif fformat == 'mp4':
+    # pip install imageio[ffmpeg]
+    # pip install imageio[pyav]
+    p.open_movie("res.mp4", framerate=16)
 
 nframe = len(pvd.time_values)
 for frame in range(nframe):
