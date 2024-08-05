a8a138bbccd86009c0d2a25f27ca2313b4b74ab1
Dongheon <zwqa1234@gmail.com>
2024-08-05 20:19:09 +0900

    Update mk_gif.py

--- Phase-Field/Tutorial_02_Cahn-Hillard_2D/mk_gif.py
+++ Phase-Field/Tutorial_02_Cahn-Hillard_2D/mk_gif.py
@@ -1,6 +1,13 @@
 import pyvista as pv
+import numpy as np
 
-pvd = pv.get_reader("simulation_results.pvd")
+load_file = "simulation_results.pvd"
+save_file = "res.mp4"
+
+def grad(arr):
+    return (np.roll(arr, -1, axis=0) - np.roll(arr, 1, axis=0))/2
+
+pvd = pv.get_reader(load_file)
 sargs = dict(height=0.40, vertical=True, position_x= 0.8, position_y=0.4, n_labels=5, title='psi', fmt='%.2f')
 mesh_kwargs = {'scalars':'psi',
               'scalar_bar_args':sargs,
@@ -8,22 +15,40 @@ mesh_kwargs = {'scalars':'psi',
               'cmap':'bwr'}
 
 p = pv.Plotter()
-fname = 'res.mp4'
-fformat = fname.split('.')[-1]
+fformat = save_file.split('.')[-1]
 if fformat == 'gif':
     # pip install imageio
-    p.open_gif("res.gif")
+    p.open_gif(save_file)
 elif fformat == 'mp4':
     # pip install imageio[ffmpeg]
     # pip install imageio[pyav]
-    p.open_movie("res.mp4", framerate=16)
+    p.open_movie(save_file, framerate=16)
 
 nframe = len(pvd.time_values)
+free_energy = []
+conc = []
 for frame in range(nframe):
     data = pvd.set_active_time_point(frame)
+    grid = pvd.read()[0]
+    arr = grid.point_data.get_array(mesh_kwargs['scalars'])
+
+    grad_x = grad(arr)
+    grad_y = grad(arr.T).T
+    grad_mag = np.sqrt(grad_x**2 + grad_y**2)
+    free_energy.append(np.sum( arr**2 * (1-arr)**2 + 0.5/2 * grad_mag))
+    conc.append(np.sum(arr)/(64**2))
+
     p.add_mesh(pvd.read()[0], show_scalar_bar=True, **mesh_kwargs)
     p.camera_position = 'xy'
     p.write_frame()
 
 p.close()
     
+import matplotlib.pyplot as plt
+
+fig,ax = plt.subplots(1,2)
+ax[0].plot(free_energy)
+ax[1].plot(conc)
+ax[1].set_ylim([0.48, 0.52])
+
+plt.show()
